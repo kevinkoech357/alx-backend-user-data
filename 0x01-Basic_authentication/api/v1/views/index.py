e2cd17b78b7ac4904da6b2d68d839e757dc42f3d
Kevin Koech <kevinkoech@proton.me>
2024-02-13 23:11:36 +0300

    add an endpoint to rest forbidden error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,11 @@ def unathorized():
     Raise a 401 error.
     """
     abort(401)
+
+
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+def forbidden():
+    """
+    Raise a 403 error.
+    """
+    abort(403)
