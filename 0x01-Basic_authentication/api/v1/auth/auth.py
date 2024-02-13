92e32358a43b3f12072b0cf0c83b3fad068d3ffd
Kevin Koech <kevinkoech@proton.me>
2024-02-13 23:36:07 +0300

    update the authorization_header method to check if Authorization is in the request header

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -38,9 +38,15 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns None.
+        Returns the value of the Authorization header in the request.
         """
-        return None
+        if request is None:
+            return None
+
+        if "Authorization" not in request.headers:
+            return None
+
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar("User"):
         """
