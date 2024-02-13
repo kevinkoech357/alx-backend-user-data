0410c29a5df0488ee2aec4d28b697c5851ae9e57
Kevin Koech <kevinkoech@proton.me>
2024-02-13 23:28:43 +0300

    modify the require_auth method to return True if path or excluded paths is None and False if path in excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,9 +17,24 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Returns False for now.
+        Returns True if path or excluded_paths is None
+        and False if path in excluded_paths.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Making the paths slash tolerant
+        path = path.rstrip("/")
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("/"):
+                excluded_path = excluded_path[:-1]
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
