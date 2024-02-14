428b1c54948ee40307bb7dc9c9bddf8a478e9815
Kevin Koech <kevinkoech@proton.me>
2024-02-14 21:15:41 +0300

    return base64 of auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ Define BasicAuth class that inherits from Auth.
 
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,4 +14,20 @@ class BasicAuth(Auth):
     Inherit from Auth.
     """
 
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Return Base64 part of the Authorization header.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # Extracting the Base64 part after 'Basic '
+        base64_token = authorization_header.split(" ")[1]
+
+        return base64_token
