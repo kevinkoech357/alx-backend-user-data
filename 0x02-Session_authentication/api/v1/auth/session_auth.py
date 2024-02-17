15a085ebe99a4538c1d01202906376b3ca621a6a
Kevin Koech <kevinkoech@proton.me>
2024-02-17 22:35:29 +0300

    add a method to create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
 """
-Define BasicAuth class that inherits from Auth.
+Define SessionAuth class that inherits from Auth.
 """
 
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Inherit from Auth.
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session id for user id
+        """
+        if user is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
