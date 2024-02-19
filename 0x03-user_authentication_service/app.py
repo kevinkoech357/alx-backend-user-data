1d61e1d1a6b6989710c9e302223a41e9f0c842a0
Kevin Koech <kevinkoech@proton.me>
2024-02-19 20:58:29 +0300

    add endpoint to register user if email hasnt already been used

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,11 +4,12 @@
 Set up a basic Flask app.
 """
 
-from flask import Flask, jsonify
-from typing import List
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -17,6 +18,29 @@ def return_json_message() -> dict:
     Return a JSON payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> dict:
+    """
+    Endpoint to register user.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify(
+            {"status": "error", "message": "Email and password are required"}
+        ), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        if user is not None:
+            return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
