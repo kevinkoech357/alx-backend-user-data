f36f9d5e7980b161421947d2ca212534129684e0
Kevin Koech <kevinkoech@proton.me>
2024-02-22 12:44:38 +0300

    refactor get_user_from_session_id method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,12 +85,13 @@ class Auth:
         """
         if session_id is None:
             return None
-        else:
-            user = self._db.find_user_by(session_id=session_id)
-            if user is None:
-                return None
-            else:
-                return user
+
+        user = self._db.find_user_by(session_id=session_id)
+
+        if user is None:
+            return None
+
+        return user
 
     def destroy_session(self, user_id: int) -> None:
         """
